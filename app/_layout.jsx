634dc49e7d0faa4bfc9e3d2fb04b70e66ebcb614
rajesh-paudel <rajeshpaudel9863@gmail.com>
2025-05-01 23:16:52 +0545

    local storage

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,17 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ThemeProvider } from "../context/ThemeContext";
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{ headerShown: false }}
-        ></Stack.Screen>
-      </Stack>
-    </SafeAreaProvider>
+    <ThemeProvider>
+      <SafeAreaProvider>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{ headerShown: false }}
+          ></Stack.Screen>
+        </Stack>
+      </SafeAreaProvider>
+    </ThemeProvider>
   );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -9,13 +9,52 @@ import {
 } from "react-native";
 import { data } from "@/data/data.js";
 import { MaterialIcons } from "@expo/vector-icons";
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { usePathname } from "expo-router";
-
+import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
+import { Octicons } from "@expo/vector-icons";
+import Animated, { LinearTransition } from "react-native-reanimated";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export default function Index() {
   const [text, setText] = useState("");
-  const [todos, setTodos] = useState(data);
+  const [todos, setTodos] = useState([]);
+  const [loaded, error] = useFonts({
+    Inter_500Medium,
+  });
+  const { colorScheme, setColorScheme, theme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem("TodoApp");
+        const storageTodos = jsonValue != null ? JSON.parse(jsonValue) : null;
+        if (storageTodos && storageTodos.length) {
+          setTodos(storageTodos);
+        } else {
+          setTodos(data);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchData();
+  }, [data]);
+
+  useEffect(() => {
+    const storeData = async () => {
+      try {
+        const jsonValue = JSON.stringify(todos);
+        await AsyncStorage.setItem("TodoApp", jsonValue);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    storeData();
+  }, [todos]);
+  if (!loaded && !error) {
+    return null;
+  }
   function handleAdd() {
     if (text.trim()) {
       setTodos([
@@ -34,8 +73,13 @@ export default function Index() {
     );
     setTodos(updatedTodos);
   }
+  const styles = createStyles(theme, colorScheme);
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}
+    >
       <View style={styles.container}>
         <View
           style={{
@@ -51,14 +95,28 @@ export default function Index() {
             onChangeText={setText}
           ></TextInput>
           <TouchableOpacity onPress={handleAdd} style={styles.addButton}>
-            Add
+            <Text style={{ color: "white", textAlign: "center" }}> Add</Text>
           </TouchableOpacity>
+          <Pressable
+            onPress={() =>
+              setColorScheme(colorScheme == "light" ? "dark" : "light")
+            }
+            style={{ marginLeft: 10 }}
+          >
+            <Octicons
+              name={colorScheme == "dark" ? "moon" : "sun"}
+              size={36}
+              color={theme.text}
+            />
+          </Pressable>
         </View>
 
-        <FlatList
+        <Animated.FlatList
           data={todos}
           showsVerticalScrollIndicator={false}
           keyExtractor={(item) => item.id.toString()}
+          itemLayoutAnimation={LinearTransition}
+          keyboardDismissMode="on-drag"
           renderItem={({ item }) => (
             <View style={styles.itemContainer}>
               <Pressable onPress={() => handleCompleted(item.id)}>
@@ -76,51 +134,57 @@ export default function Index() {
               </TouchableOpacity>
             </View>
           )}
-        ></FlatList>
+        />
       </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-  },
-  input: {
-    width: "80%",
-    borderWidth: 1,
-    borderColor: "black",
-    borderRadius: 5,
-    fontSize: 16,
-    paddingLeft: 10,
-    outlineWidth: 0,
-  },
-  addButton: {
-    color: "white",
-    height: 40,
-    width: 80,
-    backgroundColor: "blue",
-    textAlign: "center",
-    justifyContent: "center",
-    borderRadius: 6,
-  },
-  itemContainer: {
-    widht: "100%",
-    padding: 5,
-    borderBottomWidth: 1,
-    borderColor: "black",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  itemText: {
-    fontSize: 16,
-    color: "black",
-    fontWeight: "semibold",
-  },
-  completedText: {
-    textDecorationLine: "line-through",
-    color: "grey",
-  },
-});
+function createStyles(theme, colorScheme) {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 10,
+      backgroundColor: theme.background,
+    },
+    input: {
+      width: "80%",
+      color: theme.text,
+      borderWidth: 1,
+      borderColor: colorScheme === "dark" ? "white" : "black",
+      borderRadius: 5,
+      fontSize: 16,
+      paddingLeft: 10,
+      outlineWidth: 0,
+      fontFamily: "Inter_500Medium",
+    },
+    addButton: {
+      color: "white",
+      height: 50,
+      width: 80,
+      backgroundColor: theme.button,
+      textAlign: "center",
+      justifyContent: "center",
+      borderRadius: 6,
+    },
+    itemContainer: {
+      widht: "100%",
+      padding: 5,
+      borderBottomWidth: 1,
+      borderColor: "black",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    itemText: {
+      fontSize: 16,
+      color: theme.text,
+      fontWeight: "semibold",
+      fontFamily: "Inter_500Medium",
+    },
+    completedText: {
+      textDecorationLine: "line-through",
+      color: "grey",
+    },
+  });
+}
